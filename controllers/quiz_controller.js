a23bdd187dae318369585b5f4cba9f28bb135de6
Miguel Sanjuán <n3tm4n@gmail.com>
2015-08-16 20:20:05 +0200

    Author

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,6 @@ exports.answer = function(req, res) {
       res.render('quizes/answer', { respuesta: 'Incorrecto' });
    }
 };
+
+// GET /author
+exports.author = function(req,res) { res.render('author')};
