7f0250bb76fddb459afdd70e929f5c42731a0a0a
Miguel Sanjuán <n3tm4n@gmail.com>
2015-08-17 16:44:54 +0200

    Múltiples preguntas

--- models/models.js
+++ models/models.js
@@ -38,8 +38,9 @@ sequelize.sync().then(function() {
    Quiz.count().then(function (count) {
       if (count == 0) { // la tabla se inicializa sólo si está vacía
          Quiz.create({  pregunta:  'Capital de Italia',
-                        respuesta: 'Roma'
-                     })
+                        respuesta: 'Roma' })
+         Quiz.create({  pregunta:  'Capital de Portugal',
+                        respuesta: 'Lisboa' })
          .then(function() { console.log('Base de datos inicializada')});
       };
    });
