b4585473bc010d4558dba9c7a084d12ba76a1080
Miguel Sanjuán <n3tm4n@gmail.com>
2015-08-18 01:17:16 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,7 +19,7 @@ exports.new = function(req, res) {
       { pregunta: "Pregunta", respuesta: "Respuesta" }
    );
 
-   res.render('quizes/new', { quiz: quiz });
+   res.render('quizes/new', { quiz: quiz, errors: [] });
 };
 
 // GET /quizes
@@ -32,14 +32,14 @@ exports.index = function(req, res) {
    }
    models.Quiz.findAll(whereClause).then(
       function(quizes) {
-         res.render('quizes/index.ejs', { quizes: quizes });
+         res.render('quizes/index.ejs', { quizes: quizes, errors: [] });
       }
    ).catch(function(error) { next(error); });
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-   res.render('quizes/show', { quiz: req.quiz });
+   res.render('quizes/show', { quiz: req.quiz, errors: [] });
 };
 
 // GET /quizes/:id/answer
@@ -48,12 +48,26 @@ exports.answer = function(req, res) {
    if (req.query.respuesta === req.quiz.respuesta) {
       resultado = 'Correcto';
    }
-   res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
+   res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: [] });
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
    var quiz = models.Quiz.build(req.body.quiz);
+
+   quiz
+   .validate()
+   .then(
+      function(err) {
+         if (err) {
+            res.render('quizes/new', { quiz: quiz, errors: err.errors });
+         } else {
+            quiz  // save: guarda en DB campos pregunta y respuesta de quiz
+            .save({ fields: ["pregunta", "respuesta"] })
+            .then( function(){ res.redirect('/quizes') })
+         }     // res.redirect: redirección HTTP a lista de preguntas
+      }
+   );
 
    // guarda en la DB los campos pregunta y respuesta de quiz
    quiz.save({ fields: ["pregunta", "respuesta"]}).then(function() {
